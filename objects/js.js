58fcde354156c5d0b10330e5544f660a42e2a053
jamesd98355 <james.donnelly246@hotmail.com>
2022-07-07 15:55:28 +0100

    initial commit

---
+++ objects/js.js
@@ -0,0 +1,25 @@
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function() {
+        return (title + " by " + author + ", " + pages + " pages, " + read)
+    }
+
+
+    function Student(){
+
+    }
+
+    Student.prototype.sayName = function() {
+        console.log(this.name)
+    }
+
+    function EighthGrader(name){
+        this.name = name;
+        this.year = year;
+    }
+    
